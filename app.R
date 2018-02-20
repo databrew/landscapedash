0f3e53c9eaf03f67c4b5ccaa3d8e6774b26f60ed
joebrew <joebrew@gmail.com>
2018-02-20 14:20:32 +0100

    download options modal

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(shinydashboard)
 header <- dashboardHeader(title="DFS Landscape Dashboard")
 sidebar <- dashboardSidebar(
   sidebarMenu(
+    id = 'sidebar',
     menuItem(
       text='DFS Market Overview',
       tabName='dfs_market_overivew',
@@ -37,130 +38,135 @@ body <- dashboardBody(
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
   ),
-  tabItems(
-    tabItem(
-      tabName="dfs_market_overivew",
-      fluidPage()
-    ),
-    tabItem(
-      tabName="country_dashboard",
-      fluidPage(
-        tabsetPanel(
-          tabPanel('Market overview'),
-          tabPanel('Qualitative overview'),
-          tabPanel('Additional analyses')
-        )
-        
-      )
-    ),
-    tabItem(
-      tabName="country_analysis",
-      fluidPage(
-        tabsetPanel(
-          tabPanel('Recommended analyses'),
-          tabPanel('Custom analyses')
-        )
-      )
-    ),
-    tabItem(
-      tabName="x_market_analysis",
-      fluidPage(
-        tabsetPanel(
-          tabPanel('Recommended analyses'),
-          tabPanel('Custom analyses')
-        )
-      )
-    ),
-    tabItem(
-      tabName="glossary",
-      fluidPage()
-    ),
-    tabItem(
-      tabName = 'about',
-      fluidPage(
-        fluidRow(h4("The dashboard was developed as a part of activities under the ", 
-                    a(href = 'http://www.ifc.org/wps/wcm/connect/region__ext_content/ifc_external_corporate_site/sub-saharan+africa/priorities/financial+inclusion/za_ifc_partnership_financial_inclusion',
-                      target='_blank',
-                      "Partnership for Financial Inclusion"),
-                    " (a $37.4 million joint initiative of the ",
-                    a(href = "http://www.ifc.org/wps/wcm/connect/corp_ext_content/ifc_external_corporate_site/home",
-                      target='_blank',
-                      'IFC'),
-                    " and the ",
-                    a(href = "http://www.mastercardfdn.org/",
-                      target='_blank',
-                      'MasterCard Foundation'),
-                    " to expand microfinance and advance digital financial services in Sub-Saharan Africa) by the FIG Africa Digital Financial Services unit (the MEL team).")),
-        br(),
-        fluidRow(div(img(src='partnershiplogo.png', 
-                         align = "center",
-                         height = '90'), style="text-align: center;"),
-                 br(), 
-                 style = 'text-align:center;'
-        ),
-        br(),
-        fluidRow(
-          shinydashboard::box(
-            title = 'Soren Heitmann',
-            fluidPage(
-              fluidRow(
-                div(a(img(src='about/Soren Heitmann.jpg', 
-                          align = "center",
-                          height = '80'),
-                      href="mailto:sheitmann@ifc.org"), 
-                    style="text-align: center;")
-              ),
-              fluidRow(h5('Project Lead'),
-                       h5('Johannesburg, ', 
-                          a(href = 'mailto:sheitmann@ifc.org',
-                            'sheitmann@ifc.org'))),
-              fluidRow(helpText("Soren has a background in database management, software engineering and web technology. He manages the applied research and integrated monitoring, evaluation and learning program for the IFC-MasterCard Foundation Partnership for Financial Inclusion. He works at the nexus of data-driven research and technology to help drive learning and innovation within IFC’s Digital Financial Services projects in Sub-Saharan Africa."))
-            ),
-            width = 4),
-          shinydashboard::box(
-            title = 'Oleksiy Anokhin',
-            fluidPage(
-              fluidRow(
-                div(a(img(src='about/Oleksiy Anokhin.jpg', 
-                          align = "center",
-                          height = '80'),
-                      href="mailto:oanokhin@ifc.org"), 
-                    style="text-align: center;")
-              ),
-              fluidRow(h5('Project Specialist'),
-                       h5('Washington, DC, ', 
-                          a(href = 'mailto:oanokhin@ifc.org',
-                            'oanokhin@ifc.org'))),
-              fluidRow(helpText("Oleksiy focuses on data-driven visualization solutions for international development. He is passionate about using programmatic tools (such as interactive dashboards) for better planning and implementation of projects, as well as for effective communication of projects results to various stakeholders."))
-            ),
-            width = 4),
-          shinydashboard::box(
-            title = 'Joe Brew',
-            fluidPage(
-              fluidRow(
-                div(a(img(src='about/Joe Brew.png', 
-                          align = "center",
-                          height = '80'),
-                      href="mailto:jbrew1@worldbank.org"), 
-                    style="text-align: center;")
-              ),
-              fluidRow(h5('Data Scientist'),
-                       h5('Amsterdam, ', 
-                          a(href = 'mailto:jbrew1@worldbank.org',
-                            'jbrew1@worldbank.org'))),
-              fluidRow(helpText("Joe is a data scientist for", a(href = 'http://databrew.cc/', 'DataBrew.'), "He has a background in epidemiology and development economics. He works in both industry as a consultant as well as academia. His research focuses on the economics of malaria elimination programs in Sub-Saharan Africa."))
-            ),
-            width = 4)
-        ),
-        fluidRow(br(),
-                 div(a(actionButton(inputId = "email", label = "Contact", 
-                                    icon = icon("envelope", lib = "font-awesome")),
-                       href="mailto:sheitmann@ifc.org",
-                       align = 'center')), 
-                 style = 'text-align:center;'
-        )
-      )
-    )
+  fluidPage(
+    column(11,
+           tabItems(
+             tabItem(
+               tabName="dfs_market_overivew",
+               fluidPage()
+             ),
+             tabItem(
+               tabName="country_dashboard",
+               fluidPage(
+                 tabsetPanel(
+                   tabPanel('Market overview'),
+                   tabPanel('Qualitative overview'),
+                   tabPanel('Additional analyses')
+                 )
+                 
+               )
+             ),
+             tabItem(
+               tabName="country_analysis",
+               fluidPage(
+                 tabsetPanel(
+                   tabPanel('Recommended analyses'),
+                   tabPanel('Custom analyses')
+                 )
+               )
+             ),
+             tabItem(
+               tabName="x_market_analysis",
+               fluidPage(
+                 tabsetPanel(
+                   tabPanel('Recommended analyses'),
+                   tabPanel('Custom analyses')
+                 )
+               )
+             ),
+             tabItem(
+               tabName="glossary",
+               fluidPage()
+             ),
+             tabItem(
+               tabName = 'about',
+               fluidPage(
+                 fluidRow(h4("The dashboard was developed as a part of activities under the ", 
+                             a(href = 'http://www.ifc.org/wps/wcm/connect/region__ext_content/ifc_external_corporate_site/sub-saharan+africa/priorities/financial+inclusion/za_ifc_partnership_financial_inclusion',
+                               target='_blank',
+                               "Partnership for Financial Inclusion"),
+                             " (a $37.4 million joint initiative of the ",
+                             a(href = "http://www.ifc.org/wps/wcm/connect/corp_ext_content/ifc_external_corporate_site/home",
+                               target='_blank',
+                               'IFC'),
+                             " and the ",
+                             a(href = "http://www.mastercardfdn.org/",
+                               target='_blank',
+                               'MasterCard Foundation'),
+                             " to expand microfinance and advance digital financial services in Sub-Saharan Africa) by the FIG Africa Digital Financial Services unit (the MEL team).")),
+                 br(),
+                 fluidRow(div(img(src='partnershiplogo.png', 
+                                  align = "center",
+                                  height = '90'), style="text-align: center;"),
+                          br(), 
+                          style = 'text-align:center;'
+                 ),
+                 br(),
+                 fluidRow(
+                   shinydashboard::box(
+                     title = 'Soren Heitmann',
+                     fluidPage(
+                       fluidRow(
+                         div(a(img(src='about/Soren Heitmann.jpg', 
+                                   align = "center",
+                                   height = '80'),
+                               href="mailto:sheitmann@ifc.org"), 
+                             style="text-align: center;")
+                       ),
+                       fluidRow(h5('Project Lead'),
+                                h5('Johannesburg, ', 
+                                   a(href = 'mailto:sheitmann@ifc.org',
+                                     'sheitmann@ifc.org'))),
+                       fluidRow(helpText("Soren has a background in database management, software engineering and web technology. He manages the applied research and integrated monitoring, evaluation and learning program for the IFC-MasterCard Foundation Partnership for Financial Inclusion. He works at the nexus of data-driven research and technology to help drive learning and innovation within IFC’s Digital Financial Services projects in Sub-Saharan Africa."))
+                     ),
+                     width = 4),
+                   shinydashboard::box(
+                     title = 'Oleksiy Anokhin',
+                     fluidPage(
+                       fluidRow(
+                         div(a(img(src='about/Oleksiy Anokhin.jpg', 
+                                   align = "center",
+                                   height = '80'),
+                               href="mailto:oanokhin@ifc.org"), 
+                             style="text-align: center;")
+                       ),
+                       fluidRow(h5('Project Specialist'),
+                                h5('Washington, DC, ', 
+                                   a(href = 'mailto:oanokhin@ifc.org',
+                                     'oanokhin@ifc.org'))),
+                       fluidRow(helpText("Oleksiy focuses on data-driven visualization solutions for international development. He is passionate about using programmatic tools (such as interactive dashboards) for better planning and implementation of projects, as well as for effective communication of projects results to various stakeholders."))
+                     ),
+                     width = 4),
+                   shinydashboard::box(
+                     title = 'Joe Brew',
+                     fluidPage(
+                       fluidRow(
+                         div(a(img(src='about/Joe Brew.png', 
+                                   align = "center",
+                                   height = '80'),
+                               href="mailto:jbrew1@worldbank.org"), 
+                             style="text-align: center;")
+                       ),
+                       fluidRow(h5('Data Scientist'),
+                                h5('Amsterdam, ', 
+                                   a(href = 'mailto:jbrew1@worldbank.org',
+                                     'jbrew1@worldbank.org'))),
+                       fluidRow(helpText("Joe is a data scientist for", a(href = 'http://databrew.cc/', 'DataBrew.'), "He has a background in epidemiology and development economics. He works in both industry as a consultant as well as academia. His research focuses on the economics of malaria elimination programs in Sub-Saharan Africa."))
+                     ),
+                     width = 4)
+                 ),
+                 fluidRow(br(),
+                          div(a(actionButton(inputId = "email", label = "Contact", 
+                                             icon = icon("envelope", lib = "font-awesome")),
+                                href="mailto:sheitmann@ifc.org",
+                                align = 'center')), 
+                          style = 'text-align:center;'
+                 )
+               )
+             )
+           )),
+    column(1,
+           actionButton('download', 'Download', icon = icon('download')))
   )
 )
 
@@ -169,6 +175,29 @@ ui <- dashboardPage(header, sidebar, body, skin="blue")
 
 # Server
 server <- function(input, output) {
+  
+  # Observe the download button and show modal 
+  # for download optoins
+  observeEvent(input$download, {
+    showModal(modalDialog(
+      title = "Download options",
+      fluidPage(
+        fluidRow(
+          column(8,
+                 radioButtons('download_type', 'What would you like to download?',
+                              choices = c('Raw data (csv)',
+                                          'Chart (png)',
+                                          'Report (pdf)'))),
+          column(4,
+                 actionButton('download_confirm',
+                              'Download',
+                              icon = icon('download')))
+        )
+      ),
+      easyClose = TRUE,
+      footer = NULL
+    ))
+  })
   
 }
 
