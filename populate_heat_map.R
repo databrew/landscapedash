6f41cae2c97cdcb8a18ecde7c517bc1a895b717a
joebrew <joebrew@gmail.com>
2018-04-12 12:46:15 +0200

    attempts at joining values

--- populate_heat_map.R
+++ populate_heat_map.R
@@ -1,2 +1,26 @@
 # Read in all data
 source('global.R')
+
+# Read in the excel spreadsheet
+hm <- read_excel('heatmap/heatmap.xlsx', skip = 2)
+
+# # Use the column headers in hm (row 3) to reorganize the data
+# x <- df %>%
+#   filter(key %in% names(hm) | key_original %in% names(hm))
+
+# To much unmatching. Just make wide for everything
+wide <- df %>%
+  filter(year <= 2016) %>%
+  arrange(desc(year)) %>%
+  mutate(dummy = 1) %>%
+  group_by(country, key) %>%
+  mutate(dummy = cumsum(dummy)) %>%
+  filter(dummy == 1) %>%
+  summarise(value = dplyr::first(value)) %>%
+  ungroup %>%
+  spread(key = key,
+         value = value)
+
+write_csv(wide, 'heatmap_big.csv')
+
+write_csv(data.frame(key = sort(unique(df$key))), 'valid_keys.csv')
